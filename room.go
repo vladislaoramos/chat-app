adda7eab8a105d8584c6c19c2a532848b6ea4f5e
Vladislav <vladislaoramos@gmail.com>
2021-10-24 17:05:31 +0300

    Model the client and a room

---
+++ room.go
@@ -0,0 +1,13 @@
+package main
+
+type room struct {
+	// forward is a channel that holds incoming messages
+	// that should be forwarded to the other clients.
+	forward chan []byte
+	// join is a channel for clients wishing to join the room.
+	join chan *client
+	// leave is a channel for clients wishing to leave the room.
+	leave chan *client
+	// clients holds all current clients in this room.
+	clients map[*client]bool
+}
