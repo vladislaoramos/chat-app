8a9a6d4375586e05fa19860b5047eb725ea067ce
Vladislav <vladislaoramos@gmail.com>
2021-10-24 16:26:05 +0300

    Implement templateHandler using own handler

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ type templateHandler struct {
 }
 
 // ServeHTTP handles the HTTP request.
-func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r*http.Request) {
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ =  template.Must(template.ParseFiles(filepath.Join("templates",
 			t.filename)))
@@ -25,20 +25,11 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r*http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r  *http.Request) {
-		w.Write([]byte(`
-			<html>
-           		<head>
-					<title>Chat</title>
-           		</head>
-           		<body>
-             		Let's chat!
-           		</body>
-			</html>`))
-     })
+	// root
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
-     // start the web server
-     if err := http.ListenAndServe(":8080", nil); err != nil {
+	// start the web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
-	 }
+	}
 }
