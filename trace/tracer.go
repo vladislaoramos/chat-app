0871f793f4433a1fd2c2f47ba876fe325893eabe
Vladislav <vladislaoramos@gmail.com>
2021-10-24 19:03:13 +0300

    Add tracing code to get a look under the hood

---
+++ trace/tracer.go
@@ -0,0 +1,25 @@
+package trace
+
+import (
+	"fmt"
+	"io"
+)
+
+type tracer struct {
+	out io.Writer
+}
+
+// Tracer is the interface that describes an object capable of
+// tracing events throughout code.
+type Tracer interface {
+	Trace(...interface{})
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	fmt.Fprint(t.out, a...)
+	fmt.Fprintln(t.out)
+}
