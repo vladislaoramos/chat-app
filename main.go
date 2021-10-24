c1665030bdfaa5ede4a4bd636f6d0e5956b08e04
Vladislav <vladislaoramos@gmail.com>
2021-10-24 16:17:31 +0300

    Separate views from logic using templates

--- main.go
+++ main.go
@@ -3,8 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
-
+	"path/filepath"
+	"sync"
+	"text/template"
 )
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r*http.Request) {
+	t.once.Do(func() {
+		t.templ =  template.Must(template.ParseFiles(filepath.Join("templates",
+			t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r  *http.Request) {
 		w.Write([]byte(`
