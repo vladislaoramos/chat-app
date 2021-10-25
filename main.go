8a4577ecd1edf8eddc3b9cb7baf0e0de0237af57
Vladislav <vladislaoramos@gmail.com>
2021-10-25 13:44:07 +0300

    Add loginHandler

--- main.go
+++ main.go
@@ -35,6 +35,7 @@ func main() {
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	// get the room going
