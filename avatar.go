7e3b0ba5275d30b5eb26c5bc472eee8e7b57f68a
Vladislav <vladislaoramos@gmail.com>
2021-10-28 23:46:24 +0300

    Create the first Avatar implementation

--- avatar.go
+++ avatar.go
@@ -16,3 +16,16 @@ type Avatar interface {
 	// a URL for the specified client.
 	GetAvatarURL(c *client) (string, error)
 }
+
+type AuthAvatar struct{}
+
+var UseAuthAvatar AuthAvatar
+
+func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
